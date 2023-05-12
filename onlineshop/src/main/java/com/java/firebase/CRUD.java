3e6b8268c56c801900adfb4d1b662806ba922b88
taolaqa123 <s3810148@rmit.edu.vn>
2023-05-12 17:59:22 +0700

    Update CRUD.java

--- onlineshop/src/main/java/com/java/firebase/CRUD.java
+++ onlineshop/src/main/java/com/java/firebase/CRUD.java
@@ -1,5 +1,6 @@
 package com.java.firebase;
 
+import java.lang.String;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,4 +12,16 @@ public class CRUD {
     private String type;
     private String phoneid;
     private String address;
+}
+
+@Getter @Setter
+public class Product {
+    private String product_id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private String price;
+    private int discount; //1: 10%; 2: 20%; 3: 50%; 4: 80%
+    private String owner;
+    
 }
