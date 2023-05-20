2ac3dc1ff834358222fb6c760d23c0e38db03773
VinhNguyen69420 <93991856+VinhNguyen69420@users.noreply.github.com>
2023-05-20 14:56:57 +0700

    Update SignUp.js

--- onlineshop/src/main/resources/templates/SignUp.js
+++ onlineshop/src/main/resources/templates/SignUp.js
@@ -1,13 +1,20 @@
+
 let signupBtn = document.getElementById("signupBtn");
 let signinBtn = document.getElementById("signinBtn");
 let nameField = document.getElementById("nameField");
+let btnSignup = document.getElementById("btnSignup")
+let btnLogin = document.getElementById("btnLogin");
 let title = document.getElementById("title")
+
 
 signinBtn.onclick = function(){
     nameField.style.maxHeight = "0";
     title.innerText = "Sign In";
     signupBtn.classList.add("disable");
     signinBtn.classList.remove("disable");
+    btnLogin.style.visibility = "visible";
+    btnSignup.style.visibility = "hidden";
+
 }
 
 
@@ -16,4 +23,61 @@ signupBtn.onclick = function(){
     title.innerText = "Sign Up";
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable");
+     btnSignup.style.visibility = "visible";
+     btnLogin.style.visibility = "hidden";
+
 }
+ document.querySelector("#btnSignup").onclick = function () {
+
+
+       let name = document.querySelector('#name').value
+       let email = document.querySelector('#email').value
+       let password = document.querySelector('#password').value
+
+       fetch('http://localhost:8080/create', {
+           method: "post",
+           headers: {
+               'Content-Type': 'application/json'
+           },
+           body: JSON.stringify({name: name, address: email,password: password})
+       })
+       .then(response => response.json())
+              .then(json => {
+                 console.log(json);
+
+              })
+
+   }
+document.querySelector("#btnLogin").onclick = function () {
+    let name = document.querySelector('#email').value;
+    let passwordd = document.querySelector('#password').value;
+
+    fetch('http://localhost:8080/get?documentId=' + name)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(json => {
+             const { name, password } = json;
+
+                        if (password === passwordd) {
+                            // Password matches
+                            title.innerText = name;
+                        } else {
+                            // Password doesn't match
+                            alert('Invalidd credentials. Please try again.');
+                        }
+        })
+        .catch(error => {
+            // Handle the error here
+            console.error('Error:', error);
+            alert('Could not find anythding. Please try again.');
+        });
+};
+
+
+
+
+
