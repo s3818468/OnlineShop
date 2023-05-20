0526280fd3ce5f1ce71f2318735656877ef724b7
VinhNguyen69420 <93991856+VinhNguyen69420@users.noreply.github.com>
2023-05-20 14:59:35 +0700

    Update CRUD.java

--- onlineshop/src/main/java/com/java/firebase/CRUD.java
+++ onlineshop/src/main/java/com/java/firebase/CRUD.java
@@ -3,12 +3,51 @@ package com.java.firebase;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+
 public class CRUD {
     private String document_id;
     private String name;
     private String password;
     private String phoneid;
     private String address;
-}
 
+    public String getDocument_id() {
+        return document_id;
+    }
+
+    public void setDocument_id(String document_id) {
+        this.document_id = document_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneid() {
+        return phoneid;
+    }
+
+    public void setPhoneid(String phoneid) {
+        this.phoneid = phoneid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
