16eee2ce90508117464cbdffec06fef7a2097988
taolaqa123 <s3810148@rmit.edu.vn>
2023-05-20 16:39:16 +0700

    Create ProductService.java

---
+++ onlineshop/src/main/java/com/java/firebase/ProductService.java
@@ -0,0 +1,47 @@
+package com.java.firebase;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.protobuf.Api;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.Document;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class ProductService {
+    public String createProduct(Product p) throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("product").document(p.getName()).set(p);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+    public Product getProduct(String productID) throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        DocumentReference documentReference = dbFirestore.collection("product").document(productID);
+        ApiFuture<DocumentSnapshot> future = documentReference.get();
+        DocumentSnapshot document = future.get();
+        Product p;
+        if (document.exists()) {
+            p = document.toObject(Product.class);
+            return p;
+        }
+        return null;
+    }
+    public String updateProduct(Product p) throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> collectionApiFuture = dbFirestore.collection("product").document(p.getName()).set(p);
+        return collectionApiFuture.get().getUpdateTime().toString();
+    }
+    public String deleteProduct(String productID) {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<WriteResult> writeResult = dbFirestore.collection("product").document(productID).delete();
+
+        return "Item(s) deleted" + productID;
+    }
+
+}
