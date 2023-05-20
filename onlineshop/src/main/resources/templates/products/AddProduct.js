ab876813ce9c85d894e70bebc0025f72ce6d8228
taolaqa123 <s3810148@rmit.edu.vn>
2023-05-21 03:50:47 +0700

    Update AddProduct.js

--- onlineshop/src/main/resources/templates/products/AddProduct.js
+++ onlineshop/src/main/resources/templates/products/AddProduct.js
@@ -1,1 +1,29 @@
-let btnAddProduct = document.getElementByID("btnAddProduct")
+async function addProduct() {
+    let product_data = {
+    name: document.getElementByID("name").value,
+    description: document.getElementByID("desc").value,
+    imageUrl: document.getElementByID("url").value,
+    price: document.getElementByID("price").value,
+    discount: document.getElementByID("disc").value,
+    owner: "frontend"
+    }
+
+    product_data = JSON.stringify(product_data)
+    let api_link = 'http://localhost:8080/product/create'
+
+    let response=await fetch(api_link,{
+            method:"POST",
+            body:product_data,
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        let data=await response.json()
+
+        if(data.message!=null){
+            alert(data.message);
+        }
+        else{
+            displayData1(data)
+        }
+}
