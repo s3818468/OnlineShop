be6deef1bde077038c446f8bfaa6989133d7a22b
taolaqa123 <s3810148@rmit.edu.vn>
2023-05-21 21:40:35 +0700

    Update AddProduct.js

--- onlineshop/src/main/resources/templates/products/AddProduct.js
+++ onlineshop/src/main/resources/templates/products/AddProduct.js
@@ -1,4 +1,4 @@
-async function addProduct() {
+/*async function addProduct() {
     let product_data = {
     name: document.getElementByID("name").value,
     description: document.getElementByID("desc").value,
@@ -23,7 +23,50 @@ async function addProduct() {
         if(data.message!=null){
             alert(data.message);
         }
-        else{
-            displayData1(data)
-        }
+
+}*/
+let btnAddProduct = document.getElementById("btnAddProduct");
+
+/*btnAddProduct.onclick = function() {
+    let api_link='http://localhost:8080/product/create'
+    let product_data={
+        "product_id": "product_3",
+        "name": "frontend",
+        "description": "front end test",
+        "imageUrl": "url",
+        "price": "$0.00",
+        "discount": "10%",
+        "owner": "frontend"
+    }
+    product_data = JSON.stringify(product_data)
+    fetch(api_link,{
+        method:"post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: product_data
+    }
+    )
+    .then(response => {
+         if (response.ok) {
+             alert('Account created successfully!');
+             return response.json();
+         } else {
+             throw new Error('Server Error');
+         }
+     })
+     .then(json => {
+         console.log(json);
+         // Additional code for success response if needed
+     })
+     .catch(error => {
+         console.error(error);
+         if (error.message === 'Server Error') {
+             alert('An error occurred while creating the account. Please try again later.');
+         }
+    })
+}*/
+btnAddProduct.onclick = function() {
+    fetch('http://localhost:8080/product/get?productId=Lakers')
+    .then(response => console.log(response))
 }
