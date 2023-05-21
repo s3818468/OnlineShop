218463c1a5b765b05d4656dd17171fa8afebf07a
taolaqa123 <s3810148@rmit.edu.vn>
2023-05-22 01:14:29 +0700

    Update ProductService.java

--- onlineshop/src/main/java/com/java/firebase/ProductService.java
+++ onlineshop/src/main/java/com/java/firebase/ProductService.java
@@ -2,15 +2,14 @@ package com.java.firebase;
 
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutures;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.protobuf.Api;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.Document;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -20,9 +19,9 @@ public class ProductService {
         ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("product").document(p.getName()).set(p);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
-    public Product getProduct(String productID) throws ExecutionException, InterruptedException {
+    public Product getProduct(String productId) throws ExecutionException, InterruptedException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference documentReference = dbFirestore.collection("product").document(productID);
+        DocumentReference documentReference = dbFirestore.collection("product").document(productId);
         ApiFuture<DocumentSnapshot> future = documentReference.get();
         DocumentSnapshot document = future.get();
         Product p;
@@ -31,6 +30,17 @@ public class ProductService {
             return p;
         }
         return null;
+    }
+    public List<Product> getAllProduct() throws ExecutionException, InterruptedException{
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = dbFirestore.collection("product").get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        List<Product> list = new ArrayList<>();
+        for (QueryDocumentSnapshot document : documents){
+            Product temp = document.toObject(Product.class);
+            list.add(temp);
+        }
+        return list;
     }
     public String updateProduct(Product p) throws ExecutionException, InterruptedException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
