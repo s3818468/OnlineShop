a22e9c81780609e06585f38b0b8c4eb0d15aaeb8
taolaqa123 <s3810148@rmit.edu.vn>
2023-05-21 21:43:53 +0700

    Update Product.java

--- onlineshop/src/main/java/com/java/firebase/Product.java
+++ onlineshop/src/main/java/com/java/firebase/Product.java
@@ -4,7 +4,6 @@ package com.java.firebase;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
 public class Product {
     private String product_id;
     private String name;
@@ -14,4 +13,59 @@ public class Product {
     private String discount; //1: 10%; 2: 20%; 3: 50%; 4: 80%
     private String owner;
 
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
