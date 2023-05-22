26d2a53ce74a24cc2c86857dd4a816d1fed1a58c
s3818468 <105411623+s3818468@users.noreply.github.com>
2023-05-23 05:25:19 +0700

    Update SignUp.js

--- onlineshop/src/main/resources/templates/SignUp.js
+++ onlineshop/src/main/resources/templates/SignUp.js
@@ -1,82 +1,105 @@
 
-let signupBtn = document.getElementById("signupBtn");
-let signinBtn = document.getElementById("signinBtn");
-let nameField = document.getElementById("nameField");
+
+let phoneField = document.getElementById("phoneField");
 let btnSignup = document.getElementById("btnSignup")
 let btnLogin = document.getElementById("btnLogin");
 let title = document.getElementById("title")
+var signup = 0;
 
+btnLogin.onclick = function () {
+    if (signup == 0) {
+        phoneField.style.maxHeight = "0";
+        title.innerText = "Sign In";
+        btnLogin.classList.remove("disable");
+        btnSignup.classList.add("disable");
+        signup = 1;
+        
+    }
+    else if (signup == 1) {
+        let name = document.querySelector('#name').value;
+        let passwordd = document.querySelector('#password').value;
 
-signinBtn.onclick = function(){
-    nameField.style.maxHeight = "0";
-    title.innerText = "Sign In";
-    signupBtn.classList.add("disable");
-    signinBtn.classList.remove("disable");
-    btnLogin.style.visibility = "visible";
-    btnSignup.style.visibility = "hidden";
+        fetch('http://localhost:8080/get?documentId=' + name)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(json => {
+                const { name, password } = json;
 
+                if (password === passwordd) {
+                    // Password matches
+
+                    localStorage.setItem('login-state', 1);
+                    localStorage.setItem('currentUser', name);
+                    window.location.href = "index.html";
+                } else {
+                    // Password doesn't match
+                    alert('Invalid credentials. Please try again.');
+                }
+            })
+            .catch(error => {
+                // Handle the error here
+                console.error('Error:', error);
+                alert('Could not find anything. Please try again.');
+            });
+    }
 }
 
 
-signupBtn.onclick = function(){
-    nameField.style.maxHeight = "60px";
-    title.innerText = "Sign Up";
-    signupBtn.classList.remove("disable");
-    signinBtn.classList.add("disable");
-     btnSignup.style.visibility = "visible";
-     btnLogin.style.visibility = "hidden";
-
-}
- document.querySelector("#btnSignup").onclick = function () {
-
-
-       let name = document.querySelector('#name').value
-       let email = document.querySelector('#email').value
-       let password = document.querySelector('#password').value
-
-       fetch('http://localhost:8080/create', {
-           method: "post",
-           headers: {
-               'Content-Type': 'application/json'
-           },
-           body: JSON.stringify({name: name, address: email,password: password})
-       })
-       .then(response => response.json())
-              .then(json => {
-                 console.log(json);
-
-              })
-
-   }
-document.querySelector("#btnLogin").onclick = function () {
-    let name = document.querySelector('#email').value;
+btnSignup.onclick = function () {
+    if (signup == 1) {
+        phoneField.style.maxHeight = "60px";
+        title.innerText = "Sign Up";
+        btnLogin.classList.add("disable");
+        btnSignup.classList.remove("disable");
+        signup = 0;
+       
+    }
+    else if(signup == 0){
+        let name = document.querySelector('#name').value;
+    let phone = document.querySelector('#phone').value;
     let password = document.querySelector('#password').value;
+     fetch('http://localhost:8080/get?documentId=' + name)
+             .then(response => {
+                 if (!response.ok) {
+                     throw new Error('Network response was not ok');
+                 }
+                 return response.json();
+             })
+             .then(json => {
+                 alert('username already taken')
+             })
+             .catch(error => {
+                 fetch('http://localhost:8080/create', {
+                         method: "post",
+                         headers: {
+                             'Content-Type': 'application/json'
+                         },
+                         body: JSON.stringify({name: name, phoneid: phone, password: password})
+                     })
+                     .then(response => {
+                         if (response.ok) {
+                             alert('Account created successfully!');
+                             return response.json();
+                         } else {
+                             throw new Error('Server Error');
+                         }
+                     })
+                     .then(json => {
+                         console.log(json);
+                         // Additional code for success response if needed
+                     })
+                     .catch(error => {
+                         console.error(error);
+                         if (error.message === 'Server Error') {
+                             alert('An error occurred while creating the account. Please try again later.');
+                         }
+                     });
 
-    fetch('http://localhost:8080/get?documentId=' + name)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(json => {
-             const { username, password } = json;
+             });
+    }
 
-                        if (password == password) {
-                            // Password matches
-                            title.innerText = username;
-                        } else {
-                            // Password doesn't match
-                            alert('Invalid credentials. Please try again.');
-                        }
-        })
-        .catch(error => {
-            // Handle the error here
-            console.error('Error:', error);
-            alert('Could not find anything. Please try again.');
-        });
-};
-
-
-
-
+}
