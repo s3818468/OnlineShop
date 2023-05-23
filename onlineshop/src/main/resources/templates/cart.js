321117f11b59dc0a19ecfae4f0f10b537c6714f3
VinhNguyen69420 <93991856+VinhNguyen69420@users.noreply.github.com>
2023-05-23 22:52:52 +0700

    Update cart.js

--- onlineshop/src/main/resources/templates/cart.js
+++ onlineshop/src/main/resources/templates/cart.js
@@ -1,69 +1,113 @@
 const owner = localStorage.getItem('currentUser');
 const checkout = document.getElementById("checkout");
-checkout.onclick = function(){
-    alert('Thank you for your purchase, we will contact you soon');
-    window.location.href = "index.html";
-}
+let totalPrice = 0;
+
+checkout.onclick = function() {
+  alert('Thank you for your purchase, we will contact you soon');
+  window.location.href = "index.html";
+};
+
 fetch('http://localhost:8080/cart/get?username=' + owner)
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    return response.json();
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.includes('application/json')) {
+      return response.json();
+    } else {
+      throw new Error('Invalid response format');
+    }
   })
   .then(json => {
-    const productIds = json.map(cart => cart.productIds);
-    const container = document.createElement("div");
+      const cartItems = json; // Assuming the JSON response is an array of cart items
+      const container = document.createElement("div");
 
-    const fetchPromises = productIds.map(productId => {
-      return fetch('http://localhost:8080/product/get?productId=' + productId)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
+      const fetchPromises = cartItems.map(cartItem => {
+        const productId = cartItem.productIds;
+        const cartId = cartItem.cartId; // Retrieve the cartId for each cart item
+
+        return fetch('http://localhost:8080/product/get?productId=' + productId)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const contentType = response.headers.get('content-type');
+            if (contentType && contentType.includes('application/json')) {
+              return response.json();
+            } else {
+              throw new Error('Invalid response format');
+            }
+          })
+          .then(json => {
+            return {
+              productId: productId,
+              price: json.price,
+              imageUrl: json.imageUrl
+            };
+          });
+      });
+
+      Promise.all(fetchPromises)
+        .then(products => {
+          products.forEach((product, index) => {
+            const cartItem = cartItems[index]; // Get the corresponding cartItem object
+
+            const productIdElement = document.createElement("p");
+            const productPriceElement = document.createElement("p");
+            const deleteButton = document.createElement("button");
+            let image = document.createElement("img");
+            image.setAttribute("src", product.imageUrl);
+            image.setAttribute("width", "200px");
+
+            productIdElement.innerText = product.productId;
+            productPriceElement.innerText = product.price + "$";
+            totalPrice += parseFloat(product.price);
+            let total = document.getElementById('total');
+            total.innerHTML = totalPrice.toFixed(2) + "$";
+            deleteButton.innerText = "Delete";
+            deleteButton.addEventListener("click", () => {
+              var url = "http://localhost:8080/cart/delete?documentId=" + cartItem.cartId;
+              fetch(url, {
+                  method: "delete"
+                })
+                .then(response => {
+                  if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                  }
+                  const contentType = response.headers.get('content-type');
+                  if (contentType && contentType.includes('application/json')) {
+                    return response.json();
+                  } else {
+                    window.location.href = "cart.html";
+                  }
+                })
+                .then(json => {
+                window.location.href = "index.html";
+                  console.log(json);
+                })
+                .catch(error => {
+                  console.error(error);
+
+                });
+            });
+
+            console.log("Delete button clicked for product: " + product.productId);
+
+            container.appendChild(productIdElement);
+            container.appendChild(productPriceElement);
+            container.appendChild(image);
+            container.appendChild(deleteButton);
+          });
+          document.body.appendChild(container);
         })
-        .then(json => {
-          return {
-            productId: productId,
-            price: json.price,
-            imageUrl: json.imageUrl
-          };
+        .catch(error => {
+          console.error(error);
+          alert('An error occurred while retrieving the cart items. Please try again later.');
         });
+    })
+    .catch(error => {
+      console.error(error);
+      alert('An error occurred while retrieving the cart items. Please try again later.');
     });
 
-    Promise.all(fetchPromises)
-      .then(products => {
-        products.forEach(product => {
-          const productIdElement = document.createElement("p");
-          const productPriceElement = document.createElement("p");
-          const deleteButton = document.createElement("button");
-          let image = document.createElement("img");
-          image.setAttribute("src", product.imageUrl);
-          image.setAttribute("width", "200px");
-
-          productIdElement.innerText = product.productId;
-          productPriceElement.innerText = product.price;
-          deleteButton.innerText = "Delete";
-          deleteButton.addEventListener("click", () => {
-            // Handle delete action here
-            // You can make a delete request to the server or perform any other desired action
-            console.log("Delete button clicked for product: " + product.productId);
-          });
-
-          container.appendChild(productIdElement);
-          container.appendChild(productPriceElement);
-          container.appendChild(image);
-          container.appendChild(deleteButton);
-        });
-        document.body.appendChild(container);
-      })
-      .catch(error => {
-        console.error(error);
-        alert('An error occurred while retrieving the cart items. Please try again later.');
-      });
-  })
-  .catch(error => {
-    console.error(error);
-    alert('An error occurred while retrieving the cart items. Please try again later.');
-  });
